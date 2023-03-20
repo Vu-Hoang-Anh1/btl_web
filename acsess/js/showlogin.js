70b5f30fb9fe3fa0432b169024d47a3afcee5277
TRAN MINH HIEU <tranhieu02042002@gmail.com>
2023-03-20 14:11:54 +0700

    fix bug js

--- acsess/js/showlogin.js
+++ acsess/js/showlogin.js
@@ -1,20 +1,20 @@
-const dangNhap=document.querySelector('.js__member');
-const login=document.querySelector('.js__login');
-const login__close=document.querySelector('.js__login__close');
-const login__container=document.querySelector('.js__login__container');
+const dangNhap = document.querySelector('.js__member');
+const login = document.querySelector('.js__login');
+const login__close = document.querySelector('.js__login__close');
+const login__container = document.querySelector('.js__login__container');
 
-function show(){
+function show() {
     login.classList.add('open');
 }
 
-function closelogin(){
+function closelogin() {
     login.classList.remove('open');
 }
 
-dangNhap.addEventListener('click',show);
+dangNhap.addEventListener('click', show);
 
-login__close.addEventListener('click',closelogin);
-login.addEventListener('click',closelogin);
-login__container.addEventListener('click',function(event){
+login__close.addEventListener('click', closelogin);
+login.addEventListener('click', closelogin);
+login__container.addEventListener('click', function (event) {
     event.stopPropagation();
 })
